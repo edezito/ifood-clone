f925fffa3e7a67f5e7b893a4c637a9a3a0a4e015
edezito <ederduartebento@gmail.com>
2026-04-29 09:55:17 -0300

    fix: corrigir case-sensitive import HistoricoPedidos

--- src/views/Clienteapp.jsx
+++ src/views/Clienteapp.jsx
@@ -10,7 +10,7 @@ import {
 } from 'lucide-react';
 import { useClienteController } from '../controllers/useClienteController';
 import AcompanhamentoPedido from './Acompanhamentopedido';
-import HistoricoPedidos from './HistoricoPedidos';
+import HistoricoPedidos from './historicopedidos';
 import LoginCliente from './Logincliente';
 import CheckoutModal from './checkout';
 
